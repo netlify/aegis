acb54e158c302566b425c5b18d88800433389f43
Isabella Muerte <63051+slurps-mad-rips@users.noreply.github.com>
2020-06-04 10:16:57 -0700

    ✨ Improve surface area of aegis::stackable<T> constraint
    ✨ Add aegis::stack_iterator<T>
      This does not currently use the apex::mixin::iterator, but will do so
      in a future change
    ✨ aegis::stack<T> is now iterable
    🔥 Remove out of date "musing" comment on API design

--- include/aegis/stack.hpp
+++ include/aegis/stack.hpp
@@ -4,39 +4,12 @@
 #include <aegis/memory.hpp>
 #include <aegis/types.hpp>
 
-// XXX: This entire setup is EXTREMELY volatile (not the keyword, :P)
-// Effectively, OpenSSL uses macros to generate names. However, we can't
-// *reflect* on a type name with the preprocessor (that's just basic
-// rules of the translation phases). So we have to 'get' the name of
-// various types. To do this, we use AEGIS_DECLARE_STACK_TYPE_OF(TYPE)
-// to create a specialization for stack_of<T>. We can then use this to set
-// the correct type information, as we now have both T and stack_of<T>.
-// However, because of how these macros are used, we need to permit them to 
-// exist beyond *just* this header (or else we'll be including EVERY header
-// in the openssl...)
-//
-// On the bright side, once we have this type information, declaring the
-// 'interface' for a given type T becomes as simple as using the type
-// info to recreate the interface defined within openssl/safestack.h
-// We then declare all of these functions within aegis::impl::stack::,
-// and then rely on overloads to make sure functions are declared correctly.
-// We also add `void (const)*` overloads that are deleted to help prevent
-// mistakes. We'll then use a separate macro within src/stack.cxx to generate
-// the actual implementation of all these functions. Then "adding" a new type
-// becomes as simple as adding two lines. One to the file where we declare the
-// stack type, and src/stack.cxx
-//
-// An alternative approach to getting the stack type might be to just use the
-// AEGIS_DECLARE_STACK_INTERFACE(TYPE) and then extract the first parameter.
-// Personally, I think that's safer, but it requires more template
-// metaprogramming.
-
 // XXX: The push_front and push_back functions take value_type* instead of value_type const*
 // The documentation given to us by OpenSSL is incorrect
 #define AEGIS_STACK_INTERFACE(TYPE) \
   template <> struct stack_of<TYPE> : apex::type_identity<                                 \
-    std::remove_pointer_t<                                                                 \
-      std::invoke_result_t<decltype(sk_##TYPE##_new_null)>                                 \
+    apex::remove_pointer_t<                                                                \
+      apex::invoke_result_t<decltype(sk_##TYPE##_new_null)>                                \
     >                                                                                      \
   > { };                                                                                   \
   template <> struct stack_traits<TYPE> {                                                  \
@@ -57,6 +30,7 @@
     /* properties */                                                                       \
     [[nodiscard]] static value_type* get (const_pointer, size_type) noexcept;              \
     [[nodiscard]] static size_type size(const_pointer) noexcept;                           \
+    [[nodiscard]] static bool sorted (const_pointer) noexcept;                             \
                                                                                            \
     /* mutators */                                                                         \
     [[nodiscard]] static size_type reserve (pointer, size_type) noexcept;                  \
@@ -71,6 +45,7 @@
     static void push_back (pointer, value_type*) noexcept;                                 \
                                                                                            \
     static void clear (pointer) noexcept;                                                  \
+    static void sort (pointer) noexcept;                                                   \
                                                                                            \
     /* construct/destruct */                                                               \
     [[nodiscard]] static pointer construct (size_type) noexcept;                           \
@@ -78,6 +53,7 @@
     static void destruct (pointer) noexcept;                                               \
     void operator () (pointer ptr) noexcept { stack_traits::destruct(ptr); }               \
   };                                                                                       \
+  template <> struct stack_traits<TYPE const> : stack_traits<TYPE> { };                    \
   extern template struct stack_traits<TYPE>
 
 namespace aegis {
@@ -93,26 +69,114 @@ concept specialized_stack_traits = requires {
 
   typename stack_traits<T>::const_pointer;
   typename stack_traits<T>::pointer;
+
+  typename stack_traits<T>::value_type;
 };
 
-// This concept is used to make sure that the entire 'raw' interface is
-// allowed to be called, as stack<T> depends on this.
-// As a result, this should probably be changed to use typename stack_traits<T>::stack_type;
-// and friends.
-// This concept WILL grow over time and possibly break code :)
 template <class T>
 concept stackable = specialized_stack_traits<T>
-and requires (typename stack_traits<T>::pointer ref, typename stack_traits<T>::const_pointer cref) {
-  { stack_traits<T>::construct() } noexcept -> apex::same_as<typename stack_traits<T>::pointer>;
-  { stack_traits<T>::destruct(ref) } noexcept -> apex::same_as<void>;
-  { stack_traits<T>::size(cref) } noexcept -> apex::same_as<int>;
-};
+and requires (
+  typename stack_traits<T>::const_pointer cref,
+  typename stack_traits<T>::pointer ref,
+  typename stack_traits<T>::value_type* val,
+  typename stack_traits<T>::size_type n
+) {
+  { stack_traits<T>::get(cref, n) } noexcept -> apex::same_as<decltype(val)>;
+  { stack_traits<T>::size(cref) } noexcept -> apex::same_as<decltype(n)>;
+  { stack_traits<T>::sorted(cref) } noexcept -> apex::boolean;
 
+  { stack_traits<T>::reserve(ref, n) } noexcept -> apex::same_as<decltype(n)>;
+  { stack_traits<T>::erase(ref, val) } noexcept -> apex::same_as<decltype(val)>;
+  { stack_traits<T>::erase(ref, n) } noexcept -> apex::same_as<decltype(val)>;
+  { stack_traits<T>::pop_front(ref) } noexcept -> apex::same_as<decltype(val)>;
+  { stack_traits<T>::pop_back(ref) } noexcept -> apex::same_as<decltype(val)>;
+  { stack_traits<T>::push_front(ref, val) } noexcept -> apex::same_as<void>;
+  { stack_traits<T>::push_back(ref, val) } noexcept -> apex::same_as<void>;
+  { stack_traits<T>::clear(ref) } noexcept -> apex::same_as<void>;
+  { stack_traits<T>::sort(ref) } noexcept -> apex::same_as<void>;
+
+  { stack_traits<T>::construct() } noexcept -> apex::same_as<decltype(ref)>;
+  { stack_traits<T>::destruct(ref) } noexcept -> apex::same_as<void>;
+};
 
 AEGIS_STACK_INTERFACE(GENERAL_NAME);
 
+// TODO: use apex::mixin::iterator<T> once stable
 template <stackable T>
-struct stack_iterator;
+struct stack_iterator : protected view_handle<typename stack_traits<T>::stack_type> {
+  using iterator_category = std::random_access_iterator_tag;
+  using difference_type = int;
+  using value_type = T;
+  using reference = apex::add_lvalue_reference_t<value_type>;
+  using pointer = apex::add_pointer_t<value_type>;
+
+  using traits_type = stack_traits<value_type>;
+
+  stack_iterator (typename traits_type::pointer ptr, difference_type idx) noexcept :
+    view_handle<typename traits_type::stack_type> { ptr },
+    idx { idx }
+  { }
+
+  stack_iterator (stack_iterator const&) noexcept = default;
+  stack_iterator () = delete;
+
+  pointer operator -> () const noexcept {
+    return traits_type::get(this->get(), this->idx);
+  }
+
+  reference operator * () const noexcept {
+    return *traits_type::get(this->get(), this->idx);
+  }
+
+  stack_iterator operator ++ (int) noexcept {
+    stack_iterator that { *this };
+    this->idx++;
+    return that;
+  }
+
+  stack_iterator operator -- (int) noexcept {
+    stack_iterator that { *this };
+    this->idx--;
+    return that;
+  }
+
+  stack_iterator& operator ++ () noexcept {
+    ++this->idx;
+    return *this;
+  }
+
+  stack_iterator& operator -- () noexcept {
+    --this->idx;
+    return *this;
+  }
+
+  bool operator == (stack_iterator const& that) const noexcept {
+    return this->get() == that.get() and this->idx == that.idx;
+  }
+
+  bool operator != (stack_iterator const& that) const noexcept {
+    return not (*this == that);
+  }
+
+  bool operator >= (stack_iterator const& that) const noexcept {
+    return not (*this < that);
+  }
+
+  bool operator <= (stack_iterator const& that) const noexcept {
+    return not (*this > that);
+  }
+
+  bool operator > (stack_iterator const& that) const noexcept {
+    return that < *this;
+  }
+
+  bool operator < (stack_iterator const& that) const noexcept {
+    return this->get() == that.get() and this->idx < that.idx;
+  }
+
+private:
+  difference_type idx { 0 };
+};
 
 // while this will be difficult to write for "all types", we can at least
 // guarantee the iterators will "just work" for all types involved.
@@ -125,24 +189,28 @@ struct stack : private unique_handle<typename stack_traits<T>::stack_type, stack
   using value_type = typename traits_type::value_type;
   using stack_type = typename traits_type::stack_type;
   using size_type = typename traits_type::size_type;
+
+  using const_iterator = stack_iterator<apex::add_const_t<value_type>>;
+  using iterator = stack_iterator<value_type>;
+
   using handle_type = unique_handle<stack_type, traits_type>;
 
   using handle_type::handle_type;
   using handle_type::get;
-  //using const_iterator stack_iterator<apex::add_const_t<value_type>>;
-  //using iterator = stack_iterator<value_type>;
 
-  //stack (stack const&) noexcept;
-  //stack () noexcept;
+  stack (stack const&) = delete;
+  stack () = delete;
 
-  // const_iterator begin () const noexcept;
-  // const_iterator end () const noexcept;
-  // iterator begin () noexcept;
-  // iterator end () noexcept;
+  const_iterator begin () const noexcept { return { this->get(), 0 }; }
+  const_iterator end () const noexcept { return { this->get(), this->size() }; }
+
+  iterator begin () noexcept { return { this->get(), 0 }; }
+  iterator end () noexcept { return { this->get(), this->size() }; }
+
   auto operator [] (size_type idx) { return traits_type::get(this->get(), idx); }
 
   [[nodiscard]] size_type reserve (size_type n) { return traits_type::reserve(this->get(), n); }
-  [[nodiscard]] bool sorted () const noexcept;
+  [[nodiscard]] bool is_sorted () const noexcept;
 
   [[nodiscard]] size_type size () const noexcept { return traits_type::size(this->get()); }
   [[nodiscard]] bool empty () const noexcept { return this->get() and this->size(); }
