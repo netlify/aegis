c821accde6bc3a7977e25e518c32c179cf8cf30a
Isabella Muerte <63051+slurps-mad-rips@users.noreply.github.com>
2020-09-24 10:51:30 -0700

    ✨ Add aegis::error::async error codes
    ✨ Add aegis::error::asn1 error codes
    
    ♻ Refactor internal creation of error categories.
      They all use the same approach for getting an error value, so it's now
      been wrapped in a single function. We're still going to manually
      write each name and class by hand however

--- src/error.cxx
+++ src/error.cxx
@@ -3,25 +3,34 @@
 #include <apex/mixin/singleton.hpp>
 #include <openssl/err.h>
 
+namespace {
 
-namespace { namespace categories {
+/* We rely on NRVO optimizations to reduce unnecessary allocations. Do NOT
+ * call std::move. That will remove the ability to optimize this call
+ */
+::std::string to_error_string (int reason) {
+  ::std::string buffer { 512 };
+  ERR_error_string_n(reason, buffer.data(), buffer.capacity());
+  return buffer;
+}
 
-struct io final : ::std::error_category {
-  /* We rely on NRVO optimizations to reduce unnecessary allocations. Do NOT
-   * call std::move. That will remove the ability to optimize this call
-   */
-  virtual ::std::string message (int condition) const override final {
-    ::std::string buffer { 512 };
-    ERR_error_string_n(condition, buffer.data(), buffer.capacity());
-    return buffer;
-  }
+struct category : ::std::error_category {
+  virtual ::std::string message (int reason) const override final { return to_error_string(reason); }
+};
+
+struct async final : category {
+  virtual char const* name () const noexcept override final { return "openssl async"; }
+};
+
+struct io final : category {
   virtual char const* name () const noexcept override final { return "openssl io"; }
 };
 
-}} /* namespace ::categories */
+} /* nameless namespace */
 
 namespace aegis::category {
 
-::std::error_category const& io () noexcept { return apex::global<::categories::io const>; }
+::std::error_category const& async () noexcept { return apex::global<::async const>; }
+::std::error_category const& io () noexcept { return apex::global<::io const>; }
 
 } /* namespace aegis::category */
